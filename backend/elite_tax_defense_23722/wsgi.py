d6c02e6e9185af0cdf823cadf60bb7650f2e444a
Crowdbotics <team@crowdbotics.com>
2021-01-07 20:15:10 +0000

    Update models

--- backend/elite_tax_defense_23722/wsgi.py
+++ backend/elite_tax_defense_23722/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elite_tax_defense_23722.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "elite_tax_defense_23722.settings")
 
 application = get_wsgi_application()
